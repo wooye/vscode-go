e2dc27bf44bafc816ae1f7cd176745e9f95f9b96
Ramya Achutha Rao <ramyar@microsoft.com>
2018-05-12 18:52:44 -0700

    More test coverage for suggestions

--- test/go.test.ts
+++ test/go.test.ts
@@ -65,6 +65,7 @@ suite('Go Extension Tests', () => {
 		fs.copySync(path.join(fixtureSourcePath, 'linterTest', 'linter_2.go'), path.join(testPath, 'linterTest', 'linter_2.go'));
 		fs.copySync(path.join(fixtureSourcePath, 'buildTags', 'hello.go'), path.join(fixturePath, 'buildTags', 'hello.go'));
 		fs.copySync(path.join(fixtureSourcePath, 'completions', 'unimportedPkgs.go'), path.join(fixturePath, 'completions', 'unimportedPkgs.go'));
+		fs.copySync(path.join(fixtureSourcePath, 'completions', 'snippets.go'), path.join(fixturePath, 'completions', 'snippets.go'));
 		fs.copySync(path.join(fixtureSourcePath, 'importTest', 'noimports.go'), path.join(fixturePath, 'importTest', 'noimports.go'));
 		fs.copySync(path.join(fixtureSourcePath, 'importTest', 'groupImports.go'), path.join(fixturePath, 'importTest', 'groupImports.go'));
 		fs.copySync(path.join(fixtureSourcePath, 'importTest', 'singleImports.go'), path.join(fixturePath, 'importTest', 'singleImports.go'));
@@ -764,6 +765,67 @@ It returns the number of bytes written and any write error encountered.
 			assert.ok(false, `error in OpenTextDocument ${err}`);
 		}).then(() => done(), done);
 	});
+
+	test('Test Completion Snippets', (done) => {
+		let provider = new GoCompletionItemProvider();
+		let uri = vscode.Uri.file(path.join(fixturePath, 'completions', 'snippets.go'));
+		let testCases: [vscode.Position, string[]][] = [
+			[new vscode.Position(5, 6), ['Print']]
+		];
+		let baseConfig = vscode.workspace.getConfiguration('go');
+		vscode.workspace.openTextDocument(uri).then((textDocument) => {
+			return vscode.window.showTextDocument(textDocument).then(editor => {
+				
+			let noFunctionSnippet = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(8, 6), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggest': { value: false }})).then(items => {
+				let item = items.find(x => x.label === 'Print');
+				assert.equal(!item.insertText, true);
+			});
+
+			let withFunctionSnippet = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(8, 6), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggest': { value: true }})).then(items => {
+				let item = items.find(x => x.label === 'Print');
+				assert.equal((<vscode.SnippetString>item.insertText).value, 'Print(${1:a ...interface{\\}})');
+				
+			});
+
+			let withFunctionSnippetNotype = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(8, 6), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggestWithoutType': { value: true }})).then(items => {
+				let item = items.find(x => x.label === 'Print');
+				assert.equal((<vscode.SnippetString>item.insertText).value, 'Print(${1:a})');
+			});
+
+			let noFunctionAsVarSnippet = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(10, 3), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggest': { value: false }})).then(items => {
+				let item = items.find(x => x.label === 'funcAsVariable');
+				assert.equal(!item.insertText, true);
+			});
+				
+			let withFunctionAsVarSnippet = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(10, 3), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggest': { value: true }})).then(items => {
+				let item = items.find(x => x.label === 'funcAsVariable');
+				assert.equal((<vscode.SnippetString>item.insertText).value, 'funcAsVariable(${1:k string})');
+			});
+
+			let withFunctionAsVarSnippetNoType = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(10, 3), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggestWithoutType': { value: true }})).then(items => {
+				let item = items.find(x => x.label === 'funcAsVariable');
+				assert.equal((<vscode.SnippetString>item.insertText).value, 'funcAsVariable(${1:k})');
+			});
+
+			let noFunctionAsTypeSnippet = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(13, 0), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggest': { value: false }})).then(items => {
+				let item = items.find(x => x.label === 'HandlerFunc');
+				assert.equal(!item.insertText, true);
+			});
+
+			let withFunctionAsTypeSnippet = provider.provideCompletionItemsInternal(editor.document, new vscode.Position(13, 0), null, Object.create(baseConfig, {'useCodeSnippetsOnFunctionSuggest': { value: true }})).then(items => {
+				let item = items.find(x => x.label === 'HandlerFunc');
+				assert.equal((<vscode.SnippetString>item.insertText).value, 'HandlerFunc(func(${1:arg1} http.ResponseWriter, ${2:arg2} *http.Request) {\n\t$3\n})');
+			});
+
+				return Promise.all([
+					noFunctionSnippet, withFunctionSnippet, withFunctionSnippetNotype, 
+					noFunctionAsVarSnippet, withFunctionAsVarSnippet, withFunctionAsVarSnippetNoType,
+					noFunctionAsTypeSnippet, withFunctionAsTypeSnippet]).then(() => vscode.commands.executeCommand('workbench.action.closeActiveEditor'));
+			});
+		}, (err) => {
+			assert.ok(false, `error in OpenTextDocument ${err}`);
+		}).then(() => done(), done);
+	})
 
 	test('Test Completion on unimported packages', (done) => {
 		let config = Object.create(vscode.workspace.getConfiguration('go'), {
